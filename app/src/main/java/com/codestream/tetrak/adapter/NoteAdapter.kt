7208da7bf111fabaf0146e6193bc9aef5e120d80
MUradyan <hayk.muradyan@zangi.com>
2026-05-03 00:10:55 +0400

    (HM) is created NoteAdapter

--- app/src/main/java/com/codestream/tetrak/adapter/NoteAdapter.kt
+++ app/src/main/java/com/codestream/tetrak/adapter/NoteAdapter.kt
@@ -1,4 +1,37 @@
 package com.codestream.tetrak.adapter
 
-class NoteAdapter {
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.codestream.tetrak.databinding.ItemLayoutBinding
+import com.codestream.tetrak.model.NoteModel
+
+class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+    var listNote = mutableListOf<NoteModel>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val binding = ItemLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return NoteViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        holder.bind(listNote[position])
+    }
+
+    override fun getItemCount(): Int {
+        return listNote.size
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setList(list: MutableList<NoteModel>) {
+        listNote = list
+        notifyDataSetChanged()
+    }
+
+    class NoteViewHolder( val binding: ItemLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(note: NoteModel) {
+            binding.itemTitle.text = note.title
+        }
+    }
 }
